040855c03ad720232888bdb651cfc95049d6d0f3
Oscar Ibarra <oi893101@ssh.sandbox.csun.edu>
2021-02-16 23:44:56 -0800

    added read

--- checksum.c
+++ checksum.c
@@ -1,19 +1,13 @@
-/**/
-/* Program Name:                /
-/ Author:                      /
-/ Date:                        /
-/**/
-/ Description:                 /
-/ Validation Checks:           /
-/ Enhancements:                */
-/**/
 #include "stdio.h"
 #include "stdlib.h"
 #include <sys/types.h>
 #include <unistd.h>
+
 #define max_int (255)
 #define byte unsigned char
+
 int main (int argc, char * argv[], char ** envp) {
+
   int count = 10;
   int sum = 0;
   int quotient;
@@ -21,7 +15,9 @@ int main (int argc, char * argv[], char ** envp) {
   byte checksum;
   byte complement;
   byte buffer[10];
+
   read(STDIN_FILENO, &buffer, 10);
+
   for (int x = 0; x < count; x++) {
     printf("%d ", buffer[x]);
     printf("\n");
@@ -50,7 +46,3 @@ fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, comple
   }
   return 0;
 }
-
-
-
-
