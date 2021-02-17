797bcf78ce968343d085d0117dbe7d98fc6e9132
Oscar Ibarra <oi893101@ssh.sandbox.csun.edu>
2021-02-16 21:47:02 -0800

    added read

---
+++ checksum.c
@@ -0,0 +1,55 @@
+/**/
+/* Program Name: checksum       /
+/ Author: Oscar Ibarra       /
+/ Date:2/45/21                 /
+/**/
+/ Description:                 /
+/ Validation Checks:           /
+/ Enhancements:                */
+/**/
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <unistd.h>
+
+#define max_int (255)
+#define byte (unsigned char)
+
+int main (int argc, char * argv[], char ** envp) {
+
+  int count = 10;
+  int sum = 0;
+  byte checksum; 
+  byte complement;
+  byte header[10];
+
+  int quotient; int remainder;
+
+  int retval;
+
+  retval = read(STDIN_FILENO, &header, 10);
+
+  for (int c = 0; c < count ; c++)  {
+    if ( c == 5 ) {
+        checksum = header[c];
+        header[c] = 0;
+
+    }
+
+    sum +=header[c];
+  }
+
+    quotient   = sum / (max_int + 1);
+    remainder  = sum % (max_int + 1);
+    sum = quotient + remainder;
+    complement = max_int - sum;
+
+  /* the following is the prototype for the read system call /
+  / int read(int fildes, void *buf, size_t nbyte);  */
+  fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+  if (checksum != complement ) {
+    fprintf(stderr, "Error Detected!\n"); 
+    return 1;
+  }
+  return 0;
+}
