e86c0e73faaacb22edb5d79150abd95c07e29646
Oscar Ibarra <oi893101@ssh.sandbox.csun.edu>
2021-02-16 23:32:58 -0800

    starting point

--- checksum.c
+++ checksum.c
@@ -1,55 +1,56 @@
 /**/
-/* Program Name: checksum       /
-/ Author: Oscar Ibarra       /
-/ Date:2/45/21                 /
+/* Program Name:                /
+/ Author:                      /
+/ Date:                        /
 /**/
 / Description:                 /
 / Validation Checks:           /
 / Enhancements:                */
 /**/
-
 #include "stdio.h"
 #include "stdlib.h"
+#include <sys/types.h>
 #include <unistd.h>
-
 #define max_int (255)
-#define byte (unsigned char)
-
+#define byte unsigned char
 int main (int argc, char * argv[], char ** envp) {
-
   int count = 10;
   int sum = 0;
-  byte checksum; 
+  int quotient;
+  int remainder;
+  byte checksum;
   byte complement;
-  byte header[10];
-
-  int quotient; int remainder;
-
-  int retval;
-
-  retval = read(STDIN_FILENO, &header, 10);
-
-  for (int c = 0; c < count ; c++)  {
-    if ( c == 5 ) {
-        checksum = header[c];
-        header[c] = 0;
-
-    }
-
-    sum +=header[c];
+  byte buffer[10];
+  read(STDIN_FILENO, &buffer, 10);
+  for (int x = 0; x < count; x++) {
+    printf("%d ", buffer[x]);
+    printf("\n");
   }
 
-    quotient   = sum / (max_int + 1);
-    remainder  = sum % (max_int + 1);
+  for (int x = 0; x < count; x++) {
+    if (x == 5) {
+      checksum = buffer[x];
+      buffer[x] = 0;
+    }
+    sum += buffer[x];
+  }
+
+    quotient = sum / (max_int + 1);
+    remainder = sum % (max_int + 1);
     sum = quotient + remainder;
     complement = max_int - sum;
 
   /* the following is the prototype for the read system call /
   / int read(int fildes, void *buf, size_t nbyte);  */
-  fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+
+fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
   if (checksum != complement ) {
     fprintf(stderr, "Error Detected!\n"); 
     return 1;
   }
   return 0;
 }
+
+
+
+
